2672949182df5a573415ed629449ce18b01f46dd
Admir <adopilot@gmail.com>
2025-04-17 16:36:49 +0200

    Initial commit of Robot Discourse Connector

---
+++ plugin.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# name: robot-discourse-connector
+# about: Adds Chinese social share buttons to each post (Weibo, Renren, WeChat)
+# version: 0.1.0
+# authors: adopilot
+# url: https://github.com/adopilot/robot-discourse-connector
+# required_version: 3.0.0
+
+register_asset "javascripts/discourse/init-robot-discourse-connector.js", :client
+register_asset "stylesheets/common/common.scss"
