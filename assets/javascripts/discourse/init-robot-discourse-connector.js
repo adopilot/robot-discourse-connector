48ce37c0fcc6409055cda593443f1c228981e49b
Admir <adopilot@gmail.com>
2025-04-17 19:24:44 +0200

    zadnji put

--- assets/javascripts/discourse/init-robot-discourse-connector.js
+++ assets/javascripts/discourse/init-robot-discourse-connector.js
@@ -1,49 +1,59 @@
-api.modifyClass("component:post-menu", {
-  pluginId: "robot-discourse-connector",
+import { withPluginApi } from "discourse/lib/plugin-api";
 
-  postMenuButtons: [
-    {
-      action: "openUtrošeniSati",
-      icon: "check",
-      label: "Utrošeni Sati",
-    },
-    {
-      action: "openKasaProblem",
-      icon: "cart-plus",
-      label: "Novi Problem na Kasi",
-    },
-    {
-      action: "openKasaProblemClose",
-      icon: "cart-arrow-down",
-      label: "Zatvori Problem na Kasi",
-    },
-  ],
+export default {
+  name: "robot-discourse-connector",
 
-  actions: {
-    openUtrošeniSati() {
-      const link = window.location.href;
-      const title = document.title;
-      window.open(
-        `http://erp.rgt.ba/ipPref/Task?url=${encodeURIComponent(link)}&title=${encodeURIComponent(title)}`,
-        "_blank",
-        "width=1120,height=480"
-      );
-    },
-    openKasaProblem() {
-      const link = window.location.href;
-      window.open(
-        `http://core.rgt.ba/ProblemiKasa/Create?url=${encodeURIComponent(link)}`,
-        "_blank",
-        "width=800,height=600"
-      );
-    },
-    openKasaProblemClose() {
-      const link = window.location.href;
-      window.open(
-        `http://core.rgt.ba/ProblemiKasa/Zatvori?url=${encodeURIComponent(link)}`,
-        "_blank",
-        "width=800,height=600"
-      );
-    },
+  initialize() {
+    withPluginApi("1.15.0", (api) => {
+      api.modifyClass("component:post-menu", {
+        pluginId: "robot-discourse-connector",
+
+        postMenuButtons: [
+          {
+            action: "openUtrošeniSati",
+            icon: "check",
+            label: "Utrošeni Sati",
+          },
+          {
+            action: "openKasaProblem",
+            icon: "cart-plus",
+            label: "Novi Problem na Kasi",
+          },
+          {
+            action: "openKasaProblemClose",
+            icon: "cart-arrow-down",
+            label: "Zatvori Problem na Kasi",
+          },
+        ],
+
+        actions: {
+          openUtrošeniSati() {
+            const link = window.location.href;
+            const title = document.title;
+            window.open(
+              `http://erp.rgt.ba/ipPref/Task?url=${encodeURIComponent(link)}&title=${encodeURIComponent(title)}`,
+              "_blank",
+              "width=1120,height=480"
+            );
+          },
+          openKasaProblem() {
+            const link = window.location.href;
+            window.open(
+              `http://core.rgt.ba/ProblemiKasa/Create?url=${encodeURIComponent(link)}`,
+              "_blank",
+              "width=800,height=600"
+            );
+          },
+          openKasaProblemClose() {
+            const link = window.location.href;
+            window.open(
+              `http://core.rgt.ba/ProblemiKasa/Zatvori?url=${encodeURIComponent(link)}`,
+              "_blank",
+              "width=800,height=600"
+            );
+          },
+        },
+      });
+    });
   },
-});
+};
