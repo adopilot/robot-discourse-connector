9e3efc6fd244d80053a9cd38a8c662b6bcfed6fe
Admir <adopilot@gmail.com>
2025-04-17 18:26:50 +0200

    idemo lagano opet

--- assets/javascripts/discourse/init-robot-discourse-connector.js
+++ assets/javascripts/discourse/init-robot-discourse-connector.js
@@ -1,49 +1,32 @@
-import { withPluginApi } from "discourse/lib/plugin-api";
-import Sharing from "discourse/lib/sharing";
-
-export default {
-  name: "robot-discourse-connector",
-
-  initialize() {
-    withPluginApi("1.15.0", () => {
-      // Share: Utrošeni Sati (ERP)
-      Sharing.addSource({
-        id: "utroseniSati",
-        faIcon: "check",
-        label: "Utrošeni Sati",
-        generateUrl(link, title) {
-          return `http://erp.rgt.ba/ipPref/Task?url=${encodeURIComponent(link)}&title=${encodeURIComponent(title)}`;
-        },
-        shouldOpenInPopup: false,
-        popupHeight: 480,
-        popupWidth: 1120,
-      });
-
-      // Share: Novi problem na kasi
-      Sharing.addSource({
-        id: "kasaProblem",
-        faIcon: "cart-plus",
-        label: "Novi Problem na Kasi",
-        generateUrl(link, title) {
-          return `http://core.rgt.ba/ProblemiKasa/Create?url=${encodeURIComponent(link)}`;
-        },
-        shouldOpenInPopup: true,
-        popupHeight: 600,
-        popupWidth: 800,
-      });
-
-      // Share: Zatvori problem na kasi
-      Sharing.addSource({
-        id: "kasaProblemClose",
-        faIcon: "cart-arrow-down",
-        label: "Zatvori Problem na Kasi",
-        generateUrl(link, title) {
-          return `http://core.rgt.ba/ProblemiKasa/Zatvori?url=${encodeURIComponent(link)}`;
-        },
-        shouldOpenInPopup: true,
-        popupHeight: 600,
-        popupWidth: 800,
-      });
-    });
+Sharing.addSource({
+  id: 'utroseniSati',
+  faIcon: 'check', // remove the "fa-" prefix
+  generateUrl(link, title) {
+    return "http://erp.rgt.ba/ipPref/Task?url=" + encodeURIComponent(link) + "&title=" + encodeURIComponent(title);
   },
-};
+  shouldOpenInPopup: false,
+  popupHeight: 480,
+  popupWidth: 1120
+});
+
+Sharing.addSource({
+  id: 'kasaProblem',
+  faIcon: 'cart-plus', // was "fa-cart-plus"
+  generateUrl(link, title) {
+    return "http://core.rgt.ba/ProblemiKasa/Create?url=" + encodeURIComponent(link);
+  },
+  shouldOpenInPopup: true,
+  popupHeight: 600,
+  popupWidth: 800
+});
+
+Sharing.addSource({
+  id: 'kasaProblemClose',
+  faIcon: 'cart-arrow-down', // was "fa-cart-arrow-down"
+  generateUrl(link, title) {
+    return "http://core.rgt.ba/ProblemiKasa/Zatvori?url=" + encodeURIComponent(link);
+  },
+  shouldOpenInPopup: true,
+  popupHeight: 600,
+  popupWidth: 800
+});
